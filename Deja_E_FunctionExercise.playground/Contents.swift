46abb9f119892f9a6d453d1d89829cb0060b0818
dejaedwards <67969078+dejaedwards@users.noreply.github.com>
2020-08-04 08:41:50 -0700

    Correct file

---
+++ Deja_E_FunctionExercise.playground/Contents.swift
@@ -0,0 +1,27 @@
+import Cocoa
+
+var str = "Hello, Playground"
+
+func addition() {
+    let a = 12
+    let b = 13
+    let c = a + b
+    print(c)
+}
+addition()
+
+
+//Correct one
+    func addTwoNum(num1: Int, num2: Int) -> Int {
+        var total = 0
+        total = num1 + num2
+        
+        return total
+}
+    var totalNumber = addTwoNum(num1: 6, num2: 4)
+        
+
+
+
+
+
